b30f2c9e357a8e57694f13024796cb1b38c741a2
alexapekar <LolkaBolka72>
2018-08-27 10:29:45 +0200

    Implement getHoleCards method

--- Player.js
+++ Player.js
@@ -10,6 +10,16 @@ class Player {
 
   static showdown(gameState) {
   }
+
+  static getHoleCards(gameSate) {
+    const players = gameSate.players;
+    for (i = 0; i < players.length; i++) {
+      let player = players[i];
+      if (player.name === 'Lean a hand') {
+        return player.hole_cards;
+      }
+    }
+  }
 }
 
 module.exports = Player;
