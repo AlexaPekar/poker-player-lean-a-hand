d9d442e6483467c3f9c6d273025060b55610c9d5
Farago Bence <semmiertelme13@gmail.com>
2018-08-27 11:00:00 +0200

    Implemented pair in hand and decent Hand methods

--- Player.js
+++ Player.js
@@ -12,7 +12,40 @@ class Player {
 
   }
 
+  static hasDrill(allCards) {
+
+  }
+
   static showdown(gameState) {
+  }
+
+  static isDecentHand(cards) {
+
+    if (this.pairInHand(cards)) {
+      return true;
+    }
+
+    const decentCardValues = ['A', 'K', 'Q', 'J', '10'];
+    let decentCards = [];
+
+
+    for (let i = 0; i < decentCardValues.length; i++) {
+      for (let k = 0; k < cards.length; k++) {
+        if (decentCardValues[i] === cards[k].rank) {
+          decentCards.push(cards[k]);
+        }
+      }
+
+    }
+
+    if (decentCards.length > 1) {
+      return true;
+    }
+    return false;
+  }
+
+  static pairInHand(handCards) {
+    return cards[0].rank === cards[1].rank;
   }
 
 
